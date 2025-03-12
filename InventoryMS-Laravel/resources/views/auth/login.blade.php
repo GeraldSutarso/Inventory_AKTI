35cd780e8fb75c7f9f0e191d4d289cc4496703e9
Arzidan Fadilla <92257984+Arzidan24@users.noreply.github.com>
2025-03-12 08:57:59 +0700

    Update UI Login

--- InventoryMS-Laravel/resources/views/auth/login.blade.php
+++ InventoryMS-Laravel/resources/views/auth/login.blade.php
@@ -3,36 +3,57 @@
 
 <head>
     <title>Login | IMS</title>
-    <!-- Log on to codeastro.com for more projects! -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    <!-- VENDOR CSS -->
     <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/vendor/font-awesome/css/font-awesome.min.css')}}">
     <link rel="stylesheet" href="{{asset('assets/vendor/linearicons/style.css')}}">
-    <!-- MAIN CSS -->
     <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
-    <!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
     <link rel="stylesheet" href="{{asset('assets/css/demo.css')}}">
-    <!-- GOOGLE FONTS -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
-    <!-- ICONS -->
     <link rel="apple-touch-icon" sizes="76x76" href="{{asset('assets/img/apple-icon.png')}}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{asset('assets/img/favicon.png')}}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{asset('assets/img/Favicon akti.png')}}">
+    <style>
+        body {
+            background: url('{{asset('assets/img/background.jpg')}}') no-repeat center center fixed;
+            background-size: cover;
+            font-family: 'Source Sans Pro', sans-serif;
+        }
+        .auth-box {
+            background: rgba(255, 255, 255, 0.9);
+            border-radius: 8px;
+            box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+            padding: 30px;
+        }
+        .header img {
+            margin-bottom: 20px;
+        }
+        .btn-success {
+        background-color: #7A0B0B;  
+        border-color: #600900;
+        }
+        .btn-success:hover,
+        .btn-success:focus {
+        background-color: #600900;
+        border-color: #600900;
+        }
+        .alert {
+            margin-top: 10px;
+        }
+    </style>
 </head>
 
 <body>
-    <!-- WRAPPER -->
     <div id="wrapper">
         <div class="vertical-align-wrap">
             <div class="vertical-align-middle">
-                <div class="auth-box ">
+                <div class="auth-box">
                     <div class="left">
                         <div class="content">
-                            <div class="header">
-                                <!-- <div class="logo text-center"><img src="{{asset('assets/img/logo-dark.png')}}" alt="IMS Logo"></div> -->
-                                <p class="lead">Login to your account</p>
+                            <div class="header text-center">
+                                <img src="{{asset('assets/img/Favicon akti.png')}}" alt="IMS Logo" style="max-width: 100px; height: auto;">
+                                <p class="lead">Login Admin</p>
                             </div>
                             <form class="form-auth-small" method="POST" action="{{ route('login') }}">
                                 @csrf
@@ -40,17 +61,15 @@
                                     <label for="signin-email" class="control-label sr-only">Email</label>
                                     <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" id="signin-email" value="{{ old('email') }}" required placeholder="Email">
                                     @if ($errors->has('email'))
-                                    <br>
                                     <div class="alert alert-danger alert-dismissible">
                                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                                         <strong><i class="icon fa fa-ban"></i> Alert!</strong> &nbsp; {{ $errors->first('email') }}
                                     </div>
                                     @endif
                                 </div>
-                                <!-- Log on to codeastro.com for more projects! -->
                                 <div class="form-group">
                                     <label for="signin-password" class="control-label sr-only">Password</label>
-                                    <input type="password" class="form-control" name="password" id="signin-password" value="{{ old('password') }}" placeholder="Password">
+                                    <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" id="signin-password" placeholder="Password">
                                     @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
@@ -64,18 +83,13 @@
                                     </label>
                                 </div>
                                 <button type="submit" class="btn btn-success btn-lg btn-block">LOGIN</button>
-                                <!-- <div class="bottom">
-                                    @if (Route::has('password.request'))
-                                    <span class="helper-text"><i class="fa fa-lock"></i> <a href="{{ route('password.request') }}">Forgot password?</a></span>
-                                    @endif
-                                </div> -->
                             </form>
                         </div>
                     </div>
                     <div class="right">
                         <div class="overlay"></div>
                         <div class="content text">
-                            <h1 class="heading">Inventory Management System</h1>
+                            <h1 class="heading">Inventory Management System AKTI</h1>
                         </div>
                     </div>
                     <div class="clearfix"></div>
@@ -83,16 +97,15 @@
             </div>
         </div>
     </div>
-    <!-- END WRAPPER -->
-</body>
-<script type="text/javascript">
-   $(function () {
-        $('input').iCheck({
-            checkboxClass: 'icheckbox_square-blue',
-            radioClass: 'iradio_square-blue',
-            increaseArea: '20%' /* optional */
+    <script type="text/javascript">
+        $(function () {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' /* optional */
+            });
         });
-    });
-</script>
+    </script>
+</body>
 
 </html>
